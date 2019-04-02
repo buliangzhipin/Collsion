c627568c03798300d21a7eae8b7341ff299650da
Lee <q2298478324@gmail.com>
2019-04-02 20:56:22 +0900

    作る

---
+++ Assets/Scripts/SampleCollision.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SampleCollision : MonoBehaviour
+{
+    Transform transform;
+    void Start()
+    {
+        transform = this.transform;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
+
+public struct Coordination
+{
+    float x;
+    float y;
+}
